192180a082856014a1c57f042dc7210a2e93521b
Sanjana Tanna <ashish@Sanjanas-MacBook-Air.local>
2025-02-12 00:23:29 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./src/App";  // Ensure you have an App.js file
+
+ReactDOM.render(<App />, document.getElementById("root"));
